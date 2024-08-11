dfc29ec58883721d262db0081eb61a519fe9a119
Zaid Bin Adnan <zaidbinadnan@gmail.com>
2024-08-11 08:57:04 +0500

    Next JS 1st Project

---
+++ Projects/Next_JS/my-first-nextjs-project/src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <h1 className="text-blue-500 text-2xl">Hello, I am Zaid Bin Adnan and this is my first Next.js project</h1>
+   );
+ }

806f1f7bc1b62697ce36bd1fd0b35096d0bc87e2
Zaid Bin Adnan <zaidbinadnan@gmail.com>
2024-08-11 12:31:39 +0500

    Navigation of folders

---
+++ Projects/Next_JS/navigating-with-nextjs/src/app/name/address/page.tsx
@@ -0,0 +1,7 @@
+export default function GiveAddress() {
+    return (
+      <div className="to-blue-600 p-5 flex gap-5">
+            I live in Lahore, Pakistan
+      </div>
+    )
+  }
---
+++ Projects/Next_JS/navigating-with-nextjs/src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+
+import Link from 'next/link'
+
+
+
+
+export default function Home() {
+  return (
+    <div>
+          Hello World from Panaverse!
+          <br/>
+          <Link href="/name">Go to name page</Link>
+    </div>
+  )
+}
